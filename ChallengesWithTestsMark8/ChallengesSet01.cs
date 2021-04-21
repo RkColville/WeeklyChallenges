eb5db6a40535bb807281e1125188b66609301262
Robert Colville <rkcolville@gmail.com>
2021-04-21 18:20:16 -0500

    initial commit for challenge 1 & 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -7,15 +7,11 @@ namespace ChallengesWithTestsMark8
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if (num1 == num2)
-            {
                 return true;
-            }
             else
-            {
                 return false;
-            }
-        }
 
+        }
         public double Subtract(double minuend, double subtrahend)
         {
             return minuend - subtrahend;
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,58 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet02
     {
-        public bool CharacterIsALetter(char c)
+        public bool CharacterIsALetter(char c)  
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Count() % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
+            
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
-        }
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            else
+                return numbers.Max() + numbers.Min();
+        }   
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            // var list = new List<int>() { str1.Length, str2.Length };
+            //return list.Min();
+
+            if (str1.Length < str2.Length)
+                return str1.Length;
+            else
+                return str2.Length;
+        
+            
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+                return 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+                return 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    sum += numbers[i];
+            }
+            return sum;
+            
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+                return false;
+            int sum = numbers.Sum();
+            if (sum % 2 != 0)
+                return true;
+            else
+                return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+                return 0;
+            else
+                return number / 2;
         }
     }
 }
